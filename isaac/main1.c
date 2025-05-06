ce7e94ee0cb2cb56a09ae0ce5ae5417e44d4da3d
Isaac Benítez <francisco.malquin@outlook.es>
2025-05-05 19:03:55 -0500

    avance 1

--- isaac/main1.c
+++ isaac/main1.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include "funciones.h"
+ 
+#define PRODUCTOS_MAXIMO 10
 
 int main (int argc, char *argv[]) {
+    char productos[PRODUCTOS_MAXIMO][30];
+    int amount;
+    float precios[PRODUCTOS_MAXIMO];
+    llenarProductos(productos, precios, amount);
+
     return 0;
 }
